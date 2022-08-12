c82c4663e9ae163f55b4d60cdb3a9ec22f61459a
ikhsanheriyawan2404 <ikhsanheriyawan2404@gmail.com>
2022-08-12 11:11:52 +0700

    Nonactive feat activation users

--- app/Config/Auth.php
+++ app/Config/Auth.php
@@ -185,7 +185,8 @@ class Auth extends \Myth\Auth\Config\Auth
      *
      * @var string|null Name of the ActivatorInterface class
      */
-    public $requireActivation = 'Myth\Auth\Authentication\Activators\EmailActivator';
+    // public $requireActivation = 'Myth\Auth\Authentication\Activators\EmailActivator';
+    public $requireActivation = null;
 
     /**
      * --------------------------------------------------------------------
@@ -197,7 +198,8 @@ class Auth extends \Myth\Auth\Config\Auth
      *
      * @var string|null Name of the ResetterInterface class
      */
-    public $activeResetter = 'Myth\Auth\Authentication\Resetters\EmailResetter';
+    // public $activeResetter = 'Myth\Auth\Authentication\Resetters\EmailResetter';
+    public $activeResetter = false;
 
     /**
      * --------------------------------------------------------------------
