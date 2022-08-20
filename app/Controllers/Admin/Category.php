3c047a7bae8dda2d0ede1be17742856ed3c62304
ikhsanheriyawan2404 <ikhsanheriyawan2404@gmail.com>
2022-08-20 19:59:32 +0700

    Feat: crud category module

--- app/Controllers/Admin/Category.php
+++ app/Controllers/Admin/Category.php
@@ -19,6 +19,52 @@ class Category extends BaseController
 		]);
 	}
 
+	public function new()
+	{
+		return view('backend/categories/new', [
+			'title' => 'Create Category',
+		]);
+	}
+
+	public function edit($id = null)
+	{
+		return view('backend/categories/edit', [
+			'title' => 'Edit Categories',
+			'category' => $this->categories->find($id),
+		]);
+	}
+
+	public function create()
+	{
+		if (!$this->validate([
+            'name' => 'required|max_length[255]',
+        ])) {
+            return redirect()->back()->withInput();
+        }
+
+		$this->categories->insert([
+			'name' => $this->request->getPost('name'),
+			'slug' => url_title($this->request->getPost('name'), '-', true),
+		]);
+
+		return redirect()->to(site_url('admin/category'))->with('success', 'Data berhasil ditambah!');
+	}
+
+	public function update($id = null) {
+		if (!$this->validate([
+            'name' => 'required|max_length[255]',
+        ])) {
+            return redirect()->back()->withInput();
+        }
+
+		$this->categories->update($id, [
+			'name' => $this->request->getPost('name'),
+			'slug' => url_title($this->request->getPost('name'), '-', true),
+		]);
+
+		return redirect()->to(site_url('admin/category'))->with('success', 'Data berhasil ditambah!');
+	}
+
 	public function delete($id = null)
 	{
 		$this->categories->delete($id);
---
+++ app/Views/backend/categories/new.php
@@ -0,0 +1,61 @@
+<?= $this->extend('layouts/backend', compact('title')) ?>
+
+<?= $this->section('content') ?>
+
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0">Categories</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="<?= base_url('admin/category') ?>">Category</a></li>
+                    <li class="breadcrumb-item active">Create Category</li>
+                </ol>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>
+<!-- /.content-header -->
+
+<div class="container-fluid mb-3 d-flex justify-content-end">
+    <div class="row">
+        <div class="col-12">
+            <a class="btn btn-sm bg-navy" href="<?= base_url('admin/category') ?>">Kembali <i class="fa fa-arrow-left"></i></a>
+        </div>
+    </div>
+</div>
+
+<!-- Main content -->
+<section class="content">
+    <div class="container-fluid">
+        <div class="card">
+            <div class="card-header bg-navy">
+                <h3 class="card-title">Create Category</h3>
+            </div>
+            <?php $validation = \Config\Services::validation(); ?>
+            <form action="<?= base_url('admin/category') ?>" method="POST">
+                <?= csrf_field() ?>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <label for="name">Category Name</label>
+                                <input type="text" name="name" class="form-control form-control-sm  <?= $validation->hasError('name') ? 'is-invalid' : '' ?>" placeholder="Input category name" value="<?= old('name') ?>">
+                                <?php if ($validation->hasError('name')) : ?>
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong><?= $validation->getError('name') ?></strong>
+                                    </span>
+                                <?php endif ?>
+                            </div>
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-sm bg-navy float-right">Simpan</button>
+                </div>
+            </form>
+        </div>
+    </div><!-- /.container-fluid -->
+</section>
+
+<?= $this->endSection() ?>
